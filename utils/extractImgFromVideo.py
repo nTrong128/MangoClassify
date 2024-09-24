50e74ef12af61ed331eacdbb625a470cacc152a3
nTrong128 <lenhattrong128@gmail.com>
2024-09-24 20:15:01 +0700

    refactor code

--- utils/extractImgFromVideo.py
+++ utils/extractImgFromVideo.py
@@ -2,46 +2,41 @@ import cv2
 import os
 import glob
 
-# Path to the folder containing the video files
-video_folder = './video/non'
 
-# Directory to save all frames
-output_dir = './raw/non'
-os.makedirs(output_dir, exist_ok=True)
 
-# Define the frame rate skip (skip every other frame to get half the FPS, i.e., from 30 FPS to 15 FPS)
-skip_every_other_frame = 2  # Skip one frame, save the next
+video_folder = ['/unripe', '/ripe'  ,'/rotten']
+skip_every_other_frame = 2     
 
-# Get a list of all video files in the folder
-video_files = glob.glob(os.path.join(video_folder, '*.MOV'))  # Add more extensions if needed
 
-frame_index = 0  # To keep track of all frames across videos
+for folder in range(len(video_folder)):
+    input_folder = './video' + video_folder[folder]
+    output_dir = './raw' + video_folder[folder]
+    print(input_folder)
 
-# Loop through each video file
-for video_file in video_files:
-    # Open the video file
-    cap = cv2.VideoCapture(video_file)
+    os.makedirs(output_dir, exist_ok=True)
+
+    video_files = glob.glob(os.path.join(input_folder, '*.MOV')) # Can add more video formats here
+
+    frame_index = 0 
     
-    video_name = os.path.splitext(os.path.basename(video_file))[0]
+    for video_file in video_files:
+        cap = cv2.VideoCapture(video_file)
     
-    frame_count = 0
+        video_name = os.path.splitext(os.path.basename(video_file))[0]
     
-    # Process each frame in the video
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break  # If no more frames, exit the loop
+        frame_count = 0
+    
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break  
+            
+            if frame_count % skip_every_other_frame == 0:
+                frame_filename = os.path.join(output_dir, f'{video_name}_frame_{frame_index:05d}.jpg')
+                cv2.imwrite(frame_filename, frame)
+                frame_index += 1
+            
+            frame_count += 1
         
-        # Save only every other frame (i.e., skip one frame, capture the next)
-        if frame_count % skip_every_other_frame == 0:
-            # Save frames in one folder, using an overall frame index
-            frame_filename = os.path.join(output_dir, f'{video_name}_frame_{frame_index:05d}.jpg')
-            cv2.imwrite(frame_filename, frame)
-            frame_index += 1
-        
-        frame_count += 1
-    
-    # Release the video capture object
-    cap.release()
-
-print(f'Extracted frames from {len(video_files)} videos, skipping every other frame, and saved them in {output_dir}.')
+        cap.release()
+    print(f'Extracted frames from {len(video_files)} videos, skipping every other frame, and saved them in {output_dir}.')
--- utils/split_data.py
+++ utils/split_data.py
@@ -2,28 +2,24 @@ import os
 import shutil
 import random
 
-# Define paths
-original_dataset_dir = './raw/'
-base_dir = './dataset/'  # Directory where train, test, validation will be created
+input_dir = './raw/'
+out_dir = './dataset/'
+class_names = ['ripe', 'unripe', 'rotten']
 
-os.makedirs(base_dir, exist_ok=True)
+os.makedirs(out_dir, exist_ok=True)
 
-# Create directories for train, validation, test splits
-train_dir = os.path.join(base_dir, 'train')
-validation_dir = os.path.join(base_dir, 'validation')
-test_dir = os.path.join(base_dir, 'test')
+train_dir = os.path.join(out_dir, 'train')
+validation_dir = os.path.join(out_dir, 'validation')
+test_dir = os.path.join(out_dir, 'test')
 
-# Create subdirectories for each class
-for class_name in ['chin', 'non', 'song', 'thuiquac']:
+for class_name in class_names:
     os.makedirs(os.path.join(train_dir, class_name), exist_ok=True)
     os.makedirs(os.path.join(validation_dir, class_name), exist_ok=True)
     os.makedirs(os.path.join(test_dir, class_name), exist_ok=True)
 
-    # Get list of images in the class folder
-    class_dir = os.path.join(original_dataset_dir, class_name)
+    class_dir = os.path.join(input_dir, class_name)
     images = os.listdir(class_dir)
     
-    # Shuffle images
     random.shuffle(images)
     
     # Split: 80% train, 10% validation, 10% test
@@ -34,7 +30,6 @@ for class_name in ['chin', 'non', 'song', 'thuiquac']:
     validation_images = images[train_split:validation_split]
     test_images = images[validation_split:]
     
-    # Copy images to corresponding directories
     for img in train_images:
         shutil.copy(os.path.join(class_dir, img), os.path.join(train_dir, class_name))
     
